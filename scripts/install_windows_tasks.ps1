a663e63784c0c7c378c5b6d1c2c62c0e9da517d3
ChenyangZhao <2544073891@qq.com>
2026-07-18 22:15:27 +0800

    fix: handle spaces in windows monthly task

--- scripts/install_windows_tasks.ps1
+++ scripts/install_windows_tasks.ps1
@@ -22,13 +22,61 @@ Register-ScheduledTask `
   -Force
 
 $monthlyTaskName = "zotero-arxiv-daily-$UserId-monthly"
-$monthlyCommand = "cd /d `"$ProjectDir`" && `"$UvPath`" run src/zotero_arxiv_daily/main.py $UserFlag --mode monthly executor.max_paper_num=15"
-schtasks.exe /Create `
-  /TN $monthlyTaskName `
-  /SC MONTHLY `
-  /D 1 `
-  /ST 09:00 `
-  /TR "cmd.exe /c $monthlyCommand" `
-  /F | Out-Host
+$monthlyArgs = "run src/zotero_arxiv_daily/main.py $UserFlag --mode monthly executor.max_paper_num=15"
+$monthlyStart = (Get-Date -Hour 9 -Minute 0 -Second 0).ToString("yyyy-MM-ddTHH:mm:ss")
+$escapedUvPath = [Security.SecurityElement]::Escape($UvPath)
+$escapedMonthlyArgs = [Security.SecurityElement]::Escape($monthlyArgs)
+$escapedProjectDir = [Security.SecurityElement]::Escape($ProjectDir)
+$escapedMonthlyDescription = [Security.SecurityElement]::Escape("Monthly CCF Crossref/OpenAlex paper push for $UserId")
+$monthlyTaskXml = @"
+<?xml version="1.0" encoding="UTF-16"?>
+<Task version="1.4" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">
+  <RegistrationInfo>
+    <Description>$escapedMonthlyDescription</Description>
+  </RegistrationInfo>
+  <Triggers>
+    <CalendarTrigger>
+      <StartBoundary>$monthlyStart</StartBoundary>
+      <Enabled>true</Enabled>
+      <ScheduleByMonth>
+        <DaysOfMonth>
+          <Day>1</Day>
+        </DaysOfMonth>
+        <Months>
+          <January/><February/><March/><April/><May/><June/>
+          <July/><August/><September/><October/><November/><December/>
+        </Months>
+      </ScheduleByMonth>
+    </CalendarTrigger>
+  </Triggers>
+  <Principals>
+    <Principal id="Author">
+      <LogonType>InteractiveToken</LogonType>
+      <RunLevel>LeastPrivilege</RunLevel>
+    </Principal>
+  </Principals>
+  <Settings>
+    <MultipleInstancesPolicy>IgnoreNew</MultipleInstancesPolicy>
+    <DisallowStartIfOnBatteries>false</DisallowStartIfOnBatteries>
+    <StopIfGoingOnBatteries>false</StopIfGoingOnBatteries>
+    <AllowHardTerminate>true</AllowHardTerminate>
+    <StartWhenAvailable>true</StartWhenAvailable>
+    <RunOnlyIfNetworkAvailable>true</RunOnlyIfNetworkAvailable>
+    <Enabled>true</Enabled>
+    <Hidden>false</Hidden>
+  </Settings>
+  <Actions Context="Author">
+    <Exec>
+      <Command>$escapedUvPath</Command>
+      <Arguments>$escapedMonthlyArgs</Arguments>
+      <WorkingDirectory>$escapedProjectDir</WorkingDirectory>
+    </Exec>
+  </Actions>
+</Task>
+"@
+Register-ScheduledTask `
+  -TaskName $monthlyTaskName `
+  -Xml $monthlyTaskXml `
+  -Force
 
 Write-Host "Scheduled tasks installed for $UserId in $ProjectDir"
